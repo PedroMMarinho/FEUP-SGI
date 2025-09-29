a42bd3940349c7768b6dec6ded23de5c8d9b849e
Pedro Marinho <up202206854@up.pt>
2025-09-29 21:03:24 +0100

    Added poker Cards

--- pw1/MyContents.js
+++ pw1/MyContents.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { MyAxis } from './MyAxis.js';
 import { PokerTable } from './objects/PokerTable.js';
 import { PokerChip, PokerChipValue } from './objects/PokerChip.js';
+import { CardDeck } from './objects/CardDeck.js';
 
 /**
  *  This class contains the contents of out application
@@ -16,13 +17,17 @@ class MyContents {
         this.app = app
         this.axis = new MyAxis(this);
         this.pokerTable = new PokerTable(0, 0, 0);
+        this.cardDeck = new CardDeck();
+        this.card1 = this.cardDeck.create_card('D_C',0,this.pokerTable.getTableBase() + 0.02,0, true);
+        this.card1.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 2);
         this.pokerChip = new PokerChip(0, 7, 0, 0, PokerChipValue.TWENTY_FIVE);
     }
 
     initObjects() {
         this.app.scene.add(this.axis);
         this.app.scene.add(this.pokerTable);
-        this.app.scene.add(this.pokerChip);
+        //this.app.scene.add(this.pokerChip);
+        this.app.scene.add(this.card1);
     }
 
     initLights() {
@@ -32,41 +37,45 @@ class MyContents {
 
     }
 
-    initTextures() {
-        // load textures
-
-        this.poker = new THREE.TextureLoader().load('textures/pokerTable2.jpg');
+    initPokerTextures() {
+        // poker table
+        this.poker = new THREE.TextureLoader().load('textures/pokerTable2test.jpg');
         this.pokerMaterial = new THREE.MeshBasicMaterial({ map: this.poker });
-
+        // poker rest for players
         this.pokerRest = new THREE.TextureLoader().load('textures/pokerRest.jpg');
         this.pokerRest.wrapS = THREE.RepeatWrapping;
         this.pokerRest.wrapT = THREE.RepeatWrapping;
         this.pokerRest.rotation = -Math.PI / 2;
         this.pokerRestMaterial = new THREE.MeshBasicMaterial({
             map: this.pokerRest,
-            color: 0xA0522D,  // darker multiplier
+            color: 0xA0522D,  
             side: THREE.DoubleSide
         });
+        // poker feel (couro da mesa)
         this.pokerFeel = new THREE.TextureLoader().load('textures/pokerFeel.jpg');
         this.pokerFeel.wrapS = THREE.RepeatWrapping;
         this.pokerFeel.wrapT = THREE.RepeatWrapping;
         this.pokerFeel.rotation = -Math.PI / 2;
         this.pokerFeel.repeat.set(4, 4);
         this.pokerFeelMaterial = new THREE.MeshBasicMaterial({
-            map: this.pokerFeel, side: THREE.DoubleSide, color: 0x888888,   // darker multiplier
-            roughness: 0.9,
-            metalness: 0.0
+            map: this.pokerFeel, side: THREE.DoubleSide, color: 0x888888,   
         });
 
-
-
+        // assign materials to poker table
         this.pokerTable.pokerFeelMaterial = this.pokerFeelMaterial
         this.pokerTable.pokerTableMaterial = this.pokerMaterial
         this.pokerTable.pokerRestMaterial = this.pokerRestMaterial
         this.pokerTable.pokerLegMaterial = this.pokerRestMaterial
+    }
 
-        this.pokerTable.build()
+    buildObjects() {
+        this.pokerTable.build();
         this.pokerChip.build();
+    }
+
+    initTextures() {
+        // load textures
+        this.initPokerTextures();
 
     }
 
@@ -75,6 +84,7 @@ class MyContents {
      */
     init() {
         this.initTextures();
+        this.buildObjects();
         this.initLights();
         this.initObjects();
     }
