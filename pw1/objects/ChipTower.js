8a3c4c609cae6b0014cba6f89f72fd349c6a3378
Sérgio Nossa <up202206856@up.pt>
2025-10-01 18:35:10 +0100

    chips added

--- pw1/objects/ChipTower.js
+++ pw1/objects/ChipTower.js
@@ -22,17 +22,10 @@ class ChipTower extends THREE.Object3D {
     build(nChips,topMaterial, sideMaterial) {
 
         const chipGeo = new THREE.CylinderGeometry(this.chipRadius, this.chipRadius, this.chipHeight*nChips, 32,nChips);
-        console.log(sideMaterial)
-        console.log("ball")
-        console.log(topMaterial)
-        const sideTex = sideMaterial.map.clone();
-        sideTex.repeat.set(6,nChips);
-        sideMaterial.map = sideTex;
         const chip = new THREE.Mesh(chipGeo, [ sideMaterial, topMaterial, topMaterial]);
         this.position.set(this.x, this.y + nChips*this.chipHeight/2 , this.z);
 
         this.add(chip);
-        console.log("Chip tower created");
     }
 
 }
