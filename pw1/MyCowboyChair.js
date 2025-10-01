423605ff53aeeec3ecb3de6d8a852ab31c0bc1ac
Ismael Moniz <up202206871@up.pt>
2025-10-01 13:55:28 +0000

    completed chair legs

--- pw1/MyCowboyChair.js
+++ pw1/MyCowboyChair.js
@@ -11,6 +11,7 @@ export class MyCowboyChair extends THREE.Object3D {
 		this.zpos = zpos;
 		this.xxLength = 2
 		this.zzLength = 2;
+		this.thickness = 0.3;
 		this.color = color;
 
 		// chair seat
@@ -20,26 +21,28 @@ export class MyCowboyChair extends THREE.Object3D {
 			emissive: "#000000",
 			shininess: 90
 		});
-		const chairBase = new THREE.BoxGeometry(this.xxLength, 0.3, this.zzLength);
+		const chairBase = new THREE.BoxGeometry(this.xxLength, this.thickness, this.zzLength);
 		const baseMesh = new THREE.Mesh(chairBase, chairMaterial);
 		baseMesh.position.set(this.xpos, this.ypos, this.zpos);
 		this.add(baseMesh);
 
-		//front legs
+		// legs
 		this.legOffset = 0.2;
-		const fLeg = new THREE.CylinderGeometry(0.15, 0.15, 2, 24);
-		const bLeg = new THREE.TorusGeometry(3, 0.15, 14, 14, 1);
-		const fl_legMesh = new THREE.Mesh(fLeg, chairMaterial);
-		const fr_legMesh = new THREE.Mesh(fLeg, chairMaterial);
-		const bl_legMesh = new THREE.Mesh(bLeg, chairMaterial);
-		const br_legMesh = new THREE.Mesh(bLeg, chairMaterial);
+		const leg = new THREE.CylinderGeometry(0.15, 0.15, 2, 24);
+		const fl_legMesh = new THREE.Mesh(leg, chairMaterial);
+		const fr_legMesh = new THREE.Mesh(leg, chairMaterial);
+		const bl_legMesh = new THREE.Mesh(leg, chairMaterial);
+		const br_legMesh = new THREE.Mesh(leg, chairMaterial);
 
-		fl_legMesh.position.set(- (this.xxLength / 2 - this.legOffset) + xpos, this.ypos / 2 - 0.3, -(this.zzLength / 2 - this.legOffset) + zpos);
-		fr_legMesh.position.set((this.xxLength / 2 - this.legOffset) + xpos, this.ypos / 2 - 0.3, -(this.zzLength / 2 - this.legOffset) + zpos);
+		fl_legMesh.position.set(- (this.xxLength / 2 - this.legOffset) + xpos, this.ypos / 2 - this.thickness, -(this.zzLength / 2 - this.legOffset) + zpos);
+		fr_legMesh.position.set((this.xxLength / 2 - this.legOffset) + xpos, this.ypos / 2 - this.thickness, -(this.zzLength / 2 - this.legOffset) + zpos);
+		bl_legMesh.position.set(- (this.xxLength / 2 - this.legOffset) + xpos, this.ypos / 2 - this.thickness, (this.zzLength / 2 - this.legOffset) + zpos + this.thickness);
+		br_legMesh.position.set((this.xxLength / 2 - this.legOffset) + xpos, this.ypos / 2 - this.thickness, (this.zzLength / 2 - this.legOffset) + zpos + this.thickness);
+		bl_legMesh.rotation.x = br_legMesh.rotation.x = - Math.PI / 12;
 		this.add(fl_legMesh);
 		this.add(fr_legMesh);
-		this.add(br_legMesh);
 		this.add(bl_legMesh);
+		this.add(br_legMesh);
 	}
 }
 
