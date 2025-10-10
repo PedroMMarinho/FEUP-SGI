5fe33fc657795d15a01c98371906eb2b4994d859
Pedro Marinho <up202206854@up.pt>
2025-10-10 17:46:11 +0100

    Changed some things about camera

---
+++ pw2/src/core/MyGuiInterface.js
@@ -0,0 +1,67 @@
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import * as THREE from 'three';
+import { MyApp } from './MyApp.js';
+import { MyContents } from './MyContents.js';
+
+/**
+ * Custom GUI interface for the app
+ */
+class MyGuiInterface {
+    /**
+     * @param {MyApp} app The application object
+     */
+    constructor(app) {
+        this.app = app;
+        this.datgui = new GUI();
+        this.contents = null;
+    }
+
+    /**
+     * Set the contents object
+     * @param {MyContents} contents the contents object
+     */
+    setContents(contents) {
+        this.contents = contents;
+    }
+
+    /**
+     * Initialize the GUI interface
+     */
+    init() {
+        // --- Axis controls ---
+        const axisFolder = this.datgui.addFolder('Axis');
+        axisFolder
+            .add(this.contents, 'showAxis')
+            .name('Show Axis')
+            .onChange((value) => {
+                this.contents.enableAxis(value);
+            });
+
+        // --- Camera controls ---
+        const cameraFolder = this.datgui.addFolder('Camera');
+        const cameraNames = Object.keys(this.app.cameraManager.cameras);
+
+        const cameraController = cameraFolder
+            .add(this.app.cameraManager, 'activeCameraName', cameraNames)
+            .name('Active Camera');
+
+        cameraController.onChange((name) => {
+            this.app.cameraManager.setActiveCamera(name);
+        });
+
+        cameraFolder.open();
+
+        // --- Render mode controls (wireframe / fill) ---
+        const renderFolder = this.datgui.addFolder('Render Mode');
+        const renderOptions = { mode: 'Fill' };
+
+        renderFolder.add(renderOptions, 'mode', ['Fill', 'Wireframe']).name('Polygon Mode').onChange((value) => {
+            const wireframe = value === 'Wireframe';
+            this.contents.setWireframeMode(wireframe);
+        });
+
+        renderFolder.open();
+    }
+}
+
+export { MyGuiInterface };
---
+++ pw2/src/managers/CameraManager.js
@@ -0,0 +1,92 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+class CameraManager {
+    constructor(aspect, frustumSize = 20) {
+        this.aspect = aspect;
+        this.frustumSize = frustumSize;
+        this.cameras = {};
+        this.activeCameraName = null;
+        this.lastCameraName = null;
+        this.controls = null;
+
+        // Orthographic parameters
+        this.left = -frustumSize / 2 * aspect;
+        this.right = frustumSize / 2 * aspect;
+        this.top = frustumSize / 2;
+        this.bottom = -frustumSize / 2;
+        this.near = -frustumSize / 2;
+        this.far = frustumSize;
+    }
+
+    init() {
+        // Perspective camera (free movement)
+        const perspective = new THREE.PerspectiveCamera(75, this.aspect, 0.1, 1000);
+        perspective.position.set(10, 10, 3);
+        this.cameras['Free Fly'] = perspective;
+
+        // Orthographic “aquarium” camera
+        const aquariumCam = new THREE.OrthographicCamera(
+            this.left, this.right, this.top, this.bottom, this.near, this.far
+        );
+        aquariumCam.position.set(0, this.frustumSize / 4, this.frustumSize / 2);
+        aquariumCam.lookAt(new THREE.Vector3(0, 0, 0));
+        this.cameras['Aquarium View'] = aquariumCam;
+
+        // Submarine camera (placeholder)
+        const submarineCam = new THREE.PerspectiveCamera(75, this.aspect, 0.1, 1000);
+        submarineCam.position.set(0, 1, 0);
+        this.cameras['Submarine View'] = submarineCam;
+
+        this.setActiveCamera('Free Fly');
+    }
+
+    setActiveCamera(name) {
+        this.activeCameraName = name;
+        this.activeCamera = this.cameras[name];
+    }
+
+    getActiveCamera() {
+        return this.activeCamera;
+    }
+
+    update(renderer) {
+        if (this.lastCameraName !== this.activeCameraName) {
+            this.lastCameraName = this.activeCameraName;
+
+            // Create or update OrbitControls
+            if (!this.controls) {
+                this.controls = new OrbitControls(this.activeCamera, renderer.domElement);
+                this.controls.enableZoom = true;
+            } else {
+                this.controls.object = this.activeCamera;
+            }
+
+            this.onResize(renderer);
+        }
+
+        if (this.controls) this.controls.update();
+    }
+
+    onResize(renderer) {
+        if (!this.activeCamera) return;
+
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+
+        if (this.activeCamera.isPerspectiveCamera) {
+            this.activeCamera.aspect = width / height;
+        } else if (this.activeCamera.isOrthographicCamera) {
+            const aspect = width / height;
+            this.activeCamera.left = -this.frustumSize / 2 * aspect;
+            this.activeCamera.right = this.frustumSize / 2 * aspect;
+            this.activeCamera.top = this.frustumSize / 2;
+            this.activeCamera.bottom = -this.frustumSize / 2;
+        }
+
+        this.activeCamera.updateProjectionMatrix();
+        renderer.setSize(width, height);
+    }
+}
+
+export { CameraManager };
