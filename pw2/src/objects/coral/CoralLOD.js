aaad093359d10f3ce3f85d286ae7a9963e684c7b
Sérgio Nossa <up202206856@up.pt>
2025-10-25 23:45:02 +0100

    coral lod changes (not final)

---
+++ pw2/src/objects/coral/CoralLOD.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three';
+import { Coral } from "./Coral.js";
+import { CameraManager } from '../../managers/CameraManager.js';
+
+export class CoralLOD extends THREE.LOD {
+    constructor(){
+        super();
+        this.corals = [];
+        this.startDistance = 0;
+        this.distanceOffset = 20;
+        this.createLODs();
+    }
+    createLODs(){
+        const lodLevels = [
+            { complexity: 5, distance: this.startDistance },
+            { complexity: 4, distance: this.startDistance + this.distanceOffset },
+            { complexity: 3, distance: this.startDistance + 2 * this.distanceOffset }
+        ];
+
+        lodLevels.forEach((level) => {
+            const coral = new Coral(level.complexity);
+            this.addLevel(coral, level.distance);
+            this.corals.push(coral);
+        });
+    }
+
+
+}
