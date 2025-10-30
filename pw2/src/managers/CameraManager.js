985c2bb50fc9d0af7a586a36176dd4cf1e3e5576
Sérgio Nossa <up202206856@up.pt>
2025-10-30 17:56:15 +0000

    added delta time to camera

--- pw2/src/managers/CameraManager.js
+++ pw2/src/managers/CameraManager.js
@@ -3,25 +3,23 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 class CameraManager {
     constructor(aspect, keyManager, frustumSize = 20) {
-
         this.keyManager = keyManager;
-        console.log(this.keyManager);
         this.aspect = aspect;
         this.frustumSize = frustumSize;
         this.cameras = {};
         this.activeCameraName = null;
         this.lastCameraName = null;
-        this.cameraSelection = "Free Fly"; // default selection
+        this.cameraSelection = "Free Fly";
         this.controls = null;
 
-        this.moveSpeed = 0.2;
-        this.lookSpeed = 0.01;
+        this.moveSpeed = 6; 
+        this.lookSpeed = 0.5; 
 
         this.yaw = 0;
         this.pitch = 0;
 
         this.freeFlyActive = false;
-
+        this.clock = new THREE.Clock(); 
 
         // Orthographic parameters
         this.left = -frustumSize / 2 * aspect;
@@ -33,12 +31,10 @@ class CameraManager {
     }
 
     init() {
-        //TODO: CHANGE
         const perspective = new THREE.PerspectiveCamera(75, this.aspect, 0.1, 1000);
         perspective.position.set(10, 10, 3);
         this.cameras['Free Fly'] = perspective;
 
-        //TODO: CHANGE
         const aquariumCam = new THREE.OrthographicCamera(
             this.left, this.right, this.top, this.bottom, this.near, this.far
         );
@@ -46,7 +42,6 @@ class CameraManager {
         aquariumCam.lookAt(new THREE.Vector3(0, 0, 0));
         this.cameras['Aquarium View'] = aquariumCam;
 
-        //TODO: CHANGE
         const UnderwaterCam = new THREE.PerspectiveCamera(75, this.aspect, 0.1, 1000);
         UnderwaterCam.position.set(0, 2, 5);
         this.cameras['Underwater View'] = UnderwaterCam;
@@ -58,39 +53,27 @@ class CameraManager {
     }
 
     setActiveCamera(name) {
-        console.log("setActiveCamera: " + name);
-        console.log("active camera" + this.activeCameraName);
         if (this.activeCameraName === name) return;
-        console.log("Changing active camera to: " + name);
-        console.log("previous camera was: " + this.lastCameraName);
-
-        const previous = this.activeCameraName; // could be null
-
+        const previous = this.activeCameraName;
         this.lastCameraName = previous;
-
-        console.log(this.lastCameraName);
-
         this.activeCameraName = name;
         this.activeCamera = this.cameras[name];
-
-        console.log(`Active camera set to: ${name}`);
-        console.log("previous camera was: " + previous);
-
         this.changeCamera(previous, name);
     }
-
 
     getActiveCamera() {
         return this.activeCamera;
     }
 
     update(renderer, submarine) {
+        const deltaTime = this.clock.getDelta(); 
+
         if (this.activeCameraName === 'Underwater View' && !this.controls) {
             this.controls = new OrbitControls(this.activeCamera, renderer.domElement);
-            this.controls.target.set(0, 1, 0); // orbit around this point
+            this.controls.target.set(0, 1, 0);
             this.controls.enableZoom = true;
             this.controls.enablePan = false;
-            this.controls.update(); // important!
+            this.controls.update();
         } else if (this.activeCameraName !== 'Underwater View') {
             if (this.controls) {
                 this.controls.dispose();
@@ -99,15 +82,12 @@ class CameraManager {
         }
 
         if (this.controls) this.controls.update();
-        if (this.activeCameraName === 'Free Fly') this.updateFreeFly();
-        if (this.activeCameraName === 'Submarine View' && submarine) this.updateSubmarineFollow(submarine);
+        if (this.activeCameraName === 'Free Fly') this.updateFreeFly(deltaTime);
+        if (this.activeCameraName === 'Submarine View' && submarine) this.updateSubmarineFollow(submarine, deltaTime);
     }
-
-
 
     onResize(renderer) {
         if (!this.activeCamera) return;
-
         const width = window.innerWidth;
         const height = window.innerHeight;
 
@@ -126,40 +106,32 @@ class CameraManager {
     }
 
     changeCamera(oldName, newName) {
-        // Aquarium is always static
         if (newName === 'Aquarium View') {
-            console.log("Aquarium View here");
             const cam = this.cameras['Aquarium View'];
             cam.position.set(0, this.frustumSize / 4, this.frustumSize / 2);
             cam.lookAt(0, 10, 0);
         }
 
-        // Free Fly resets
         if (oldName === 'Free Fly') {
             this.freeFlyActive = false;
         }
-
     }
 
-    updateSubmarineFollow(submarine) {
+    updateSubmarineFollow(submarine, deltaTime) {
         const camera = this.cameras['Submarine View'];
-
         const offset = new THREE.Vector3(0, 3, 12);
-
         const relativeOffset = offset.clone().applyMatrix4(submarine.matrixWorld);
 
-        camera.position.lerp(relativeOffset, 0.1);
+        camera.position.lerp(relativeOffset, 2 * deltaTime);
 
         const lookAtOffset = new THREE.Vector3(0, 1, -5).applyMatrix4(submarine.matrixWorld);
         camera.lookAt(lookAtOffset);
     }
 
-    updateFreeFly() {
-
+    updateFreeFly(deltaTime) {
         const keyManager = this.keyManager;
-
         const camera = this.activeCamera;
-        const moveSpeed = this.moveSpeed;
+        const moveSpeed = this.moveSpeed * deltaTime; 
 
         const { x: deltaX, y: deltaY } = keyManager.getDelta();
 
@@ -167,8 +139,8 @@ class CameraManager {
         this.pitch = camera.rotation.x;
 
         if (keyManager.isMousePressed()) {
-            this.yaw -= deltaX * this.lookSpeed;
-            this.pitch -= deltaY * this.lookSpeed;
+            this.yaw -= deltaX * this.lookSpeed * deltaTime;
+            this.pitch -= deltaY * this.lookSpeed * deltaTime;
         }
 
         const limit = Math.PI / 2 - 0.01;
@@ -183,7 +155,6 @@ class CameraManager {
 
         camera.getWorldDirection(forward);
         forward.normalize();
-
         right.crossVectors(forward, camera.up).normalize();
 
         if (keyManager.isKeyPressed('KeyW')) camera.position.addScaledVector(forward, moveSpeed);
@@ -192,7 +163,6 @@ class CameraManager {
         if (keyManager.isKeyPressed('KeyD')) camera.position.addScaledVector(right, moveSpeed);
         if (keyManager.isKeyPressed('Space')) camera.position.y += moveSpeed;
         if (keyManager.isKeyPressed('ShiftLeft')) camera.position.y -= moveSpeed;
-
     }
 
     togglePointerLock() {
