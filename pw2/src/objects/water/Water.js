0f3fb5e9308913891353cfe6ff8062c7759ce471
Sérgio Nossa <up202206856@up.pt>
2025-11-08 23:53:55 +0000

    water shader added

---
+++ pw2/src/objects/water/Water.js
@@ -0,0 +1,62 @@
+import * as THREE from 'three'; 
+
+class Water extends THREE.Object3D {
+    constructor(heightMap, width, height, depth) {
+        super();
+        this.clock = new THREE.Clock(); 
+        const geometry = new THREE.PlaneGeometry(width, depth,64,64);
+        heightMap.wrapS = heightMap.wrapT = THREE.RepeatWrapping;
+        heightMap.repeat.set(4, 4);
+        this.material = new THREE.MeshPhongMaterial({
+            color: 0x1ca3ec,
+            transparent: true,
+            opacity: 0.8,
+            side: THREE.DoubleSide,
+            onBeforeCompile: (shader) => {
+                shader.uniforms.time = { value: 0 };
+                shader.uniforms.heightMap = { value: heightMap };
+                shader.uniforms.uMaxOffset = { value: 4.0 };
+
+                shader.vertexShader = `
+                    uniform float time;
+                    uniform sampler2D heightMap;
+                    uniform float uMaxOffset;
+                    varying vec2 vUv;
+
+                    ${shader.vertexShader}
+                `.replace(
+                    `#include <uv_vertex>`,
+                    `#include <uv_vertex>
+                     vUv = uv;`
+                
+                ).replace(
+                    '#include <begin_vertex>',
+                    `
+                    #include <begin_vertex>
+                    vec3 color = texture2D(heightMap, uv + vec2(time * 0.01, time * 0.01)).rgb;
+
+                    float offsetR = (color.r - 0.5) * uMaxOffset;
+                    float offsetG = (color.g - 0.5) * uMaxOffset * 0.5; 
+                    float offsetB = (color.b - 0.5) * uMaxOffset * 0.8;
+
+                    transformed += normal * (offsetR + offsetG + offsetB);`
+                );
+
+                this.material.userData.shader = shader;
+
+            }
+        });
+        this.waterSurface = new THREE.Mesh(geometry, this.material);
+        this.add(this.waterSurface);
+        this.position.set(0, height , 0);
+        this.rotation.x = -Math.PI / 2;
+    }
+    updateState(){
+        const elapsed = this.clock.getElapsedTime();
+        if(this.material.userData.shader) {
+            this.material.userData.shader.uniforms.time.value = elapsed;
+        }
+    }
+}
+
+export { Water };
