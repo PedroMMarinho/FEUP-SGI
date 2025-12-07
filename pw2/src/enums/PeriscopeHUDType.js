8c5dece4696057b003e9c5f4c73287e78bca4c9e
Pedro Marinho <up202206854@up.pt>
2025-12-07 20:49:36 +0000

    Added logic for the shadders now just need to improve them

---
+++ pw2/src/enums/PeriscopeHUDType.js
@@ -0,0 +1,10 @@
+const PeriscopeHUDType = Object.freeze({
+    VIEW: 'View',
+    DOF: 'DOF',
+    TINT: 'Tint',
+    SCRATCHES: 'Scratches',
+    HUD: 'HUD',
+    CLIP: 'Clip'
+});
+
+export { PeriscopeHUDType };
