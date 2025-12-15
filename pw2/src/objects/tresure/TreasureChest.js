593f135302c468bc73aec32b334022f0e55ed361
Pedro Marinho <up202206854@up.pt>
2025-12-15 23:32:54 +0000

    Added new rock to seabed and fixed item lod rotations

--- pw2/src/objects/tresure/TreasureChest.js
+++ pw2/src/objects/tresure/TreasureChest.js
@@ -11,12 +11,6 @@ class TreasureChest extends THREE.Object3D {
 
     getModel() {
         const model = this.gltf.scene.clone();
-        model.position.y -= 0.06;
-        model.scale.set(0.04, 0.04, 0.04);
-        // apply random rotation around Y axis
-        model.rotation.y = Math.random() * Math.PI * 2;
-        // slight tilt for more natural look
-        model.rotation.z = Math.random() * 0.1 - 0.05;
         this.changeVideoTexture(model);
         return model;
     }
--- pw2/src/objects/tv/TVLOD.js
+++ pw2/src/objects/tv/TVLOD.js
@@ -17,8 +17,11 @@ export class TVLOD extends THREE.LOD {
 
 	init() {
 		const lodNumber = this.lods.length;
+
 		for (let level = 0; level < lodNumber; level++) {
 			const tv = new TV(this.lods[level]);
+			tv.rotateX(Math.PI/2);
+			tv.position.y += 0.5;
 			this.addLevel(tv, this.distanceStart + this.distanceOffset * level);
 		}
 
