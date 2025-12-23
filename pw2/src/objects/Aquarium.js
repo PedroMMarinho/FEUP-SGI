7603796e8115e2731d26a1d743c84140b2e69535
Pedro Marinho <up202206854@up.pt>
2025-12-23 12:57:50 +0000

    Added LOD to gui for particles

--- pw2/src/objects/Aquarium.js
+++ pw2/src/objects/Aquarium.js
@@ -33,6 +33,7 @@ class Aquarium extends THREE.Object3D {
         this.cameraManager.setAquariumWidth(this.terrainWidth);
 
         this.shadowsEnabled = true;
+        this.particlesUseLOD = false;
 
         this.objects = [];
         this.envMap = null;
@@ -97,7 +98,7 @@ class Aquarium extends THREE.Object3D {
     }
 
     addMarineSnow() {
-    const marineSnow = new MarineSnow(
+    this.marineSnow = new MarineSnow(
         600, 
         {
             width: this.terrainWidth,
@@ -118,7 +119,7 @@ class Aquarium extends THREE.Object3D {
             simulateDistance: 50,
         },
     );
-    this.addToAquarium(marineSnow);
+    this.addToAquarium(this.marineSnow);
     }
 
     setupBVH() {
